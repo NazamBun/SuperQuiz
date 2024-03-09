5de1da433f61c93e62dff003f5b8727c843b7aee
Bun Nazam <bun.nazam@gmail.com>
2024-03-10 00:52:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/superquiz/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.superquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.example.superquiz.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.getRoot());
+    }
+}
